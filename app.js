40effc5864cf935348ebee8cdce03927ee3ed3d3
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2019-09-05 08:09:47 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var simonButtons=[
+  {"color":"green", "audioLink":"https://s3.amazonaws.com/freecodecamp/simonSound1.mp3", "clickedColor":""},
+  {"color":"red", "audioLink":"https://s3.amazonaws.com/freecodecamp/simonSound2.mp3", "clickedColor":""},
+  {"color":"yellow", "audioLink":"https://s3.amazonaws.com/freecodecamp/simonSound3.mp3", "clickedColor":""},
+  {"color":"blue", "audioLink":"https://s3.amazonaws.com/freecodecamp/simonSound4.mp3", "clickedColor":""}
+                ];
+
+// alert(simonButtons[3].color); blue
+//alert(simonButtons[3].audioLink)
+//alert(simonButtons[0][0]) does not work
